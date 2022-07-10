28214eebe336fb028237aa242db8e3c920deca8b
siena <nueees@gmail.com>
2022-07-10 17:56:10 +0200

    opsgenie

--- lambda/opsgenie_import_bq/opsgenie_import_bq.py
+++ lambda/opsgenie_import_bq/opsgenie_import_bq.py
@@ -2,20 +2,9 @@
 
 from datetime import date, timedelta
 from datetime import datetime
-from dh_gcs_lib import gcs_lib
-import requests
-import opsgenie_sdk
-from opsgenie_sdk.api.alert.list_alerts_response import ListAlertsResponse
-from opsgenie_sdk.api.alert.alert import Alert
-from opsgenie_sdk.api.alert.get_alert_response import GetAlertResponse
-from opsgenie_sdk.api.alert.base_alert import BaseAlert
-from opsgenie_sdk.api.alert.alert_log import AlertLog
-from opsgenie_sdk.models.page_details import PageDetails
-from requests.models import Response
 import random
 import json
 
-      
 ###################################################################################################
 #                                    handler
 ###################################################################################################      
@@ -28,3 +17,4 @@ def handler(event, context):
 
 if __name__ == '__main__':
     handler(None, None)
+
