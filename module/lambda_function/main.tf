cb2ee8f7c134ca26ecc3801b7072d8e849597d33
siena <nueees@gmail.com>
2022-07-10 19:48:05 +0200

    env_vars

--- module/lambda_function/main.tf
+++ module/lambda_function/main.tf
@@ -12,6 +12,13 @@ resource "aws_lambda_function" "lambda_function_container" {
     security_group_ids = var.security_groups
   }
 
+  dynamic "environment" {
+    for_each = local.environment_map
+    content {
+      variables = environment.value
+    }
+  }
+
   tags = var.dh_tags
 }
 
